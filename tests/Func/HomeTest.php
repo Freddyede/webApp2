517bcfe5190e9c8da5175ea6c554797bb2803c22
Franck <patouillardfranck3@gmail.com>
2022-06-05 18:07:39 +0200

    change to function testFailed to testVerifyUrl
    Signed-off-by: Franck <patouillardfranck3@gmail.com>

--- tests/Func/HomeTest.php
+++ tests/Func/HomeTest.php
@@ -6,10 +6,9 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class HomeTest extends WebTestCase
 {
-    public function testFailed()
+    public function testVerifyUrl()
     {
-        // This calls KernelTestCase::bootKernel(), and creates a
-        // "client" that is acting as the browser
+
         $client = static::createClient();
 
         // Request a specific page
