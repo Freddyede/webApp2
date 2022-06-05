e36520baf07c39c131c5e8b464fa6275393e963d
Franck <patouillardfranck3@gmail.com>
2022-06-05 18:01:04 +0200

    up composer
    Signed-off-by: Franck <patouillardfranck3@gmail.com>

---
+++ tests/Func/HomeTest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Tests\Func;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class HomeTest extends WebTestCase
+{
+    public function testFailed()
+    {
+        // This calls KernelTestCase::bootKernel(), and creates a
+        // "client" that is acting as the browser
+        $client = static::createClient();
+
+        // Request a specific page
+        $client->request('GET', '/');
+
+        // Validate a successful response and some content
+        $this->assertResponseIsSuccessful();
+        $this->assertSelectorTextContains('h1', 'Hello World');
+    }
+}
---
+++ tests/Unit/HomeTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+class HomeTest extends TestCase
+{
+    public function testFailed()
+    {
+        $this->assertEquals(2, 1 + 1);
+    }
+}
